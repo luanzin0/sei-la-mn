23c6fdee5aef8c8550820d510de56ff4e26c9424
luanzin0 <142353699+luanzin0@users.noreply.github.com>
2024-11-04 08:21:37 -0300

    a

--- projeto2.js
+++ projeto2.js
@@ -3,7 +3,7 @@ function criaCartao(categoria, pergunta, resposta) {
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
     cartao.innerHTML = `
-    <div class="conteudo-cartao">
+    <div class="conteudo-cartao" onclick="mostraResposta(this)">
         <h3>${categoria}</h3>
         <div class="pergunta-cartao">
             <p>${pergunta}</p>
@@ -15,4 +15,15 @@ function criaCartao(categoria, pergunta, resposta) {
     `;
 
     container.appendChild(cartao);
+
+    function mostraResposta(card) {
+        const resposta = card.qurySelector('.resposta-cartao')
+
+        if (resposta.styles.display === 'none')
+             { resposta.styles.display = 'block' }
+    }
+    else{
+        resposta.styles.display = 'none'
+    }
+
 }
